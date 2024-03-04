f9dc6edfb4b5099d01b836e26d4e5a62edbf5d55
Eometheous <jonathan.sthomas@icloud.com>
2024-03-04 15:53:17 -0800

    flask now uses render_template to grab html for front end

--- dev/web_app/flask/app.py
+++ dev/web_app/flask/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "こんにちは世界！"
+    return render_template('index.html')
