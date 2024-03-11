4dc4e09648f941e784096212078944736a044ec5
Eometheous <jonathan.sthomas@icloud.com>
2024-03-10 22:44:39 -0700

    added sign_up and login html files and route

--- dev/web_app/flask/app.py
+++ dev/web_app/flask/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/user/sign_up")
+def sign_up():
+    return render_template('user/sign_up.html')
+
+@app.route("/user/login")
+def login():
+    return render_template('user/login.html')
