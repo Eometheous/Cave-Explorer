f077dcbc99a29aab4e2ab2918ff3474dc7cbecb4
Eometheous <jonathan.sthomas@icloud.com>
2024-03-17 19:52:36 -0700

    connected backend to a docker container of cassandra

--- dev/web_app/flask/app.py
+++ dev/web_app/flask/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from cassandra.cluster import Cluster
 
 app = Flask(__name__)
+cluster = Cluster(['0.0.0.0'], port=9042)
+session = cluster.connect(keyspace="cave_explorer")
 
 @app.route("/")
 def home():
