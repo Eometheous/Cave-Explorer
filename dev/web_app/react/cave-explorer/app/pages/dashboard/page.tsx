b2e7ddd438b1110a74b14b662ef0e4d97dcead83
Eometheous <jonathan.sthomas@icloud.com>
2024-04-09 08:58:44 -0700

    added page for game in dashboard

---
+++ dev/web_app/react/cave-explorer/app/pages/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function game() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <iframe 
+            src="https://itch.io/embed-upload/9969638?color=333333" width="960" height="660"
+            >
+            </iframe>
+        </main>
+    );
+}
