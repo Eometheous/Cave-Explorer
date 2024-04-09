ced9dd8a79739f76d6e793a2c758e6c689e27955
Eometheous <jonathan.sthomas@icloud.com>
2024-04-09 10:56:52 -0700

    updated magic push to be more intuitive

--- dev/Cave Explorer/Assets/MagicPush.cs
+++ dev/Cave Explorer/Assets/MagicPush.cs
@@ -5,8 +5,8 @@ public class Drag : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector3 clickedPosition;
-    
     Vector2 force;
+    bool clicked;
 
     // Start is called before the first frame update
     void Start()
@@ -17,21 +17,26 @@ public class Drag : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (clicked) {
+            force = Camera.main.ScreenToWorldPoint(Input.mousePosition) - clickedPosition;
+            if (Math.Abs(force.x) > Math.Abs(force.y)) force.y = 0;
+            else force.x = 0;
+            if (rb.CompareTag("Player")) {
+                rb.AddForce(force * 100);
+            }
+            else rb.AddForce(force * 10000);
+            clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
     }
 
     void OnMouseDown()
     {
         clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        clicked = true;
     }
 
     void OnMouseUp() 
     {
-        force = Camera.main.ScreenToWorldPoint(Input.mousePosition) - clickedPosition;
-        if (Math.Abs(force.x) > Math.Abs(force.y)) force.y = 0;
-        else force.x = 0;
-        if (rb.CompareTag("Player")) {
-            rb.AddForce(force * 100);
-        }
-        else rb.AddForce(force * 10000);
+        clicked = false;
     }
 }
