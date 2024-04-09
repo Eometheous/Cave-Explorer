c9700ec23837f31f43572d32e1248af160e4419d
Eometheous <jonathan.sthomas@icloud.com>
2024-04-09 09:14:12 -0700

    updated widget

--- dev/web_app/react/cave-explorer/app/page.tsx
+++ dev/web_app/react/cave-explorer/app/page.tsx
@@ -3,10 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe 
-        src="https://itch.io/embed-upload/9969638?color=333333" width="960" height="660"
-        >
-      </iframe>
+      <iframe src="https://itch.io/embed-upload/10138290?color=333333" width="960" height="660"></iframe>
     </main>
   );
 }
