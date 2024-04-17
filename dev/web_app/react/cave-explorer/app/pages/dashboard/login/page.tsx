aed024f61c9c87d5deda157897de6566d8e305c0
Eometheous <jonathan.sthomas@icloud.com>
2024-04-17 11:57:31 -0700

    created log in page front end

--- dev/web_app/react/cave-explorer/app/pages/dashboard/login/page.tsx
+++ dev/web_app/react/cave-explorer/app/pages/dashboard/login/page.tsx
@@ -1,5 +1,44 @@
 export default function login() {
     return (
-        <p>Log In Page</p>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <form className="space-y-3">
+                <div className="flex-1 rounded-lg px-6 pb-4 pt-8">
+                    <h1>Please log in to continue.</h1>
+                    <div className="w-full">
+                        <div>
+                            <label htmlFor="email">
+                                Email
+                            </label>
+                            <div className="relative">
+                                <input 
+                                    className="peer block w-full rounded-md border border-gray-200"
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    placeholder="Enter your email address"
+                                    required
+                                />
+                            </div>
+                            <label htmlFor="email">
+                                Password
+                            </label>
+                            <div className="relative">
+                                <input 
+                                    className="peer block w-full rounded-md border border-gray-200"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    placeholder="Enter your password"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <button className="peer block w-full rounded-md border border-gray-200">
+                            Log In
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </main>
     );
 }
