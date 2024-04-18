0eb8b54974be1d1e0f23266c818952393a7752c1
Eometheous <jonathan.sthomas@icloud.com>
2024-04-17 22:15:10 -0700

    fixed redirect

--- dev/web_app/react/cave-explorer/app/api/add-user/route.ts
+++ dev/web_app/react/cave-explorer/app/api/add-user/route.ts
@@ -1,7 +1,5 @@
 import { sql } from "@vercel/postgres";
-import { url } from "inspector";
-// import { redirect } from "next/dist/server/api-utils";
-import { redirect } from 'next/navigation'
+
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -16,8 +14,8 @@ export async function GET(request: Request) {
     const hashedPass = await bcrypt.hash(password, 10);
 
     const result = await sql`INSERT INTO users (name, email, password) VALUES (${userName}, ${email}, ${hashedPass});`; 
-    redirect('/pages/dashboard/login')
-    return NextResponse.json({ result }, { status: 200 });
+
+    return NextResponse.redirect(new URL('/pages/dashboard/login', request.url))
     
    } catch (error) {
     return NextResponse.json( { error }, { status: 500 });
