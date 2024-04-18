1f8387bbd640300196bbc61e3c098866404673bd
Eometheous <jonathan.sthomas@icloud.com>
2024-04-17 22:29:31 -0700

    Updated game build

--- dev/web_app/react/cave-explorer/app/page.tsx
+++ dev/web_app/react/cave-explorer/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe src="https://itch.io/embed-upload/10182166?color=333333" width="960" height="660"></iframe>
+      <iframe src="https://itch.io/embed-upload/10212639?color=333333" width="960" height="660"></iframe>
     </main>
   );
 }
--- dev/web_app/react/cave-explorer/app/pages/dashboard/page.tsx
+++ dev/web_app/react/cave-explorer/app/pages/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function game() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <iframe src="https://itch.io/embed-upload/10182166?color=333333" width="960" height="660"></iframe>
+            <iframe src="https://itch.io/embed-upload/10212639?color=333333" width="960" height="660"></iframe>
         </main>
     );
 }
