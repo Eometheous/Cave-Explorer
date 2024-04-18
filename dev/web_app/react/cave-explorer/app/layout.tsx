6ae1d78f41a937b51e3aed8d0c26aa7099bc4e28
Eometheous <jonathan.sthomas@icloud.com>
2024-04-18 09:06:04 -0700

    changed link redirect for what is cave explorer

--- dev/web_app/react/cave-explorer/app/layout.tsx
+++ dev/web_app/react/cave-explorer/app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
         </Link>
 
         <Link
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
