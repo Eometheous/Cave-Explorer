cf06cb2b32900d955d455c4bbf698fff4f2ffb58
Eometheous <jonathan.sthomas@icloud.com>
2024-04-26 13:10:43 -0700

    fixed redirects when logged in

--- dev/web_app/react/cave-explorer/app/pages/dashboard/layout.tsx
+++ dev/web_app/react/cave-explorer/app/pages/dashboard/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <Analytics />
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
-          href="/"
+          href="/dashboard"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
@@ -37,7 +37,7 @@ export default function RootLayout({
         </Link>
 
         <Link
-          href="/"
+          href="/dashboard"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
@@ -53,7 +53,7 @@ export default function RootLayout({
         </Link>
 
         <a
-          href="/api/log-out"
+          href="/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
@@ -64,7 +64,7 @@ export default function RootLayout({
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Log into your account to continue where you left off.
+            Log out of Cave Explorer.
           </p>
         </a>
 
