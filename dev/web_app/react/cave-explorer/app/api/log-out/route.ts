b03944c03da3a6d6bb5940c723ee3cfa444585eb
Eometheous <jonathan.sthomas@icloud.com>
2024-04-26 10:12:49 -0700

    copied file contents from sign in

---
+++ dev/web_app/react/cave-explorer/app/api/log-out/route.ts
@@ -0,0 +1,23 @@
+import { sql } from "@vercel/postgres";
+
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+   const { searchParams } = new URL(request.url);
+   const userName = searchParams.get('name');
+   const email = searchParams.get('email');
+   const password = searchParams.get('password');
+
+   try {
+    if (!userName || !email || !password) throw new Error('Username, email, and password required');
+    const bcrypt = require('bcrypt');
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const result = await sql`INSERT INTO users (name, email, password) VALUES (${userName}, ${email}, ${hashedPass});`; 
+
+    return NextResponse.redirect(new URL('/pages/dashboard/login', request.url))
+    
+   } catch (error) {
+    return NextResponse.json( { error }, { status: 500 });
+   }
+}
