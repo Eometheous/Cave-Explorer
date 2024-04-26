37306c2e59d8ec3df34c1bdfc97b9508732da786
Eometheous <jonathan.sthomas@icloud.com>
2024-04-26 13:13:23 -0700

    fixed redirect for sure this time 100%

--- dev/web_app/react/cave-explorer/app/pages/dashboard/layout.tsx
+++ dev/web_app/react/cave-explorer/app/pages/dashboard/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <Analytics />
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
-          href="/dashboard"
+          href="/pages/dashboard"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
@@ -37,7 +37,7 @@ export default function RootLayout({
         </Link>
 
         <Link
-          href="/dashboard"
+          href="/pages/dashboard"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
