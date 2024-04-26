c0dafd40c500f2ba0507dc2046567610221e9511
Eometheous <jonathan.sthomas@icloud.com>
2024-04-26 13:05:06 -0700

    fixed log out

--- dev/web_app/react/cave-explorer/app/pages/dashboard/layout.tsx
+++ dev/web_app/react/cave-explorer/app/pages/dashboard/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         </Link>
 
         <a
-          href="/api/log-out"
+          href="/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           rel="noopener noreferrer"
         >
