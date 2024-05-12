03a6bd0d3e4091da938a1f3b1ad8199ffd286c29
Eometheous <jonathan.sthomas@icloud.com>
2024-05-12 10:47:01 -0700

    created level manager script

---
+++ dev/Cave Explorer/Assets/Scenes/LoadGame.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{
+    private static LoadGame instance;
+    public string email;
+    public string password;
+
+    // [SerializeField] TextMeshProUGUI usernameText;
+    // [SerializeField] TextMeshProUGUI passwordText;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GrabUsernameText(string email)
+    {
+        this.email = email;   
+    }
+    public void GrabPasswordText(string password)
+    {
+        this.password = password;
+    }
+
+    public void OnSubmitButton() 
+    {
+        // username = usernameText.text;
+        // password = passwordText.text;
+    }
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadMainMenu() 
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadLogin() 
+    {
+        SceneManager.LoadScene(1);
+    }
+}
