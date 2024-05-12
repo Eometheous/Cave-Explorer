22b9e2d4fcb50565da411e079b077039a78350d5
Eometheous <jonathan.sthomas@icloud.com>
2024-05-12 11:07:39 -0700

    updated route

--- dev/web_app/react/cave-explorer/app/api/login/route.ts
+++ dev/web_app/react/cave-explorer/app/api/login/route.ts
@@ -7,25 +7,28 @@ interface LoginRequestBody  {
     email: string;
     password: string;
 }
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const email = searchParams.get('email');
-    const password = searchParams.get('password');
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const {email, password} = req.body as LoginRequestBody;
 
-    try {
-        if (!email || !password) throw new Error('Username, email, and password required');
+        try {
+            if (!email || !password) throw new Error('Email and password are required');
 
-        const result = await sql`SELECT * FROM users WHERE email=${email}`;
+            const result = await sql`SELECT * FROM users WHERE email=${email}`;
+            const user = result.rows[0];
 
-        const user = result.rows[0];
+            if (!user) throw new Error('Email or password is inccorect');
 
-        const passwordMatch = await compare(password, user.password)
+            const passwordMatch = await compare(password, user.password);
 
-        if (user && passwordMatch) {
-            return NextResponse.redirect(new URL('/pages/dashboard/', request.url))
+            if (!passwordMatch) throw new Error('Email or password is inccorect');
+
+            res.status(200).json({success: true, message: 'Login successful', user: { email: user.email, level: user.level } });
+        } catch (error) {
+            res.status(401).json({ success: false, message: error});
         }
-        return NextResponse.redirect(new URL('/pages/auth/login', request.url))
-    } catch (error) {
-        return NextResponse.redirect(new URL('/pages/auth/login', request.url))
+    }
+    else {
+        res.status(405).json({ success: false, message: 'Method Not Allowed' });
     }
 }
