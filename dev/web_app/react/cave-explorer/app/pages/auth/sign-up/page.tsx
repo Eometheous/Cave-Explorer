8d69cdf528f39bd67b211034cec390af177e56b2
Eometheous <jonathan.sthomas@icloud.com>
2024-05-12 16:31:45 -0700

    reverted sign up page

--- dev/web_app/react/cave-explorer/app/pages/auth/sign-up/page.tsx
+++ dev/web_app/react/cave-explorer/app/pages/auth/sign-up/page.tsx
@@ -1,45 +1,9 @@
-import { useState } from 'react';
 
-export default function SignUpForm() {
-    const [formData, setFormData] = useState({
-        email: '',
-        name: '',
-        password: ''
-    });
-
-    const handleChange = (e: { target: { name: any; value: any; }; }) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-
-    const handleSubmit = async (e: { preventDefault: () => void; }) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://127.0.0.1:8080/user/sign-up', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            });
-            if (response.ok) {
-                // Handle successful form submission, e.g., show a success message
-                console.log('Form submitted successfully');
-            } else {
-                // Handle form submission error, e.g., show an error message
-                console.error('Form submission failed');
-            }
-        } catch (error) {
-            console.error('Error submitting form:', error);
-        }
-    };
+export default function sign_up() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <form onSubmit={handleSubmit} className="space-y-3">
+            <form action={"/api/add-user"} className="space-y-3">
                 <div className="flex-1 rounded-lg px-6 pb-4 pt-8">
                     <h1>Please enter your information below</h1>
                     <div className="w-full">
@@ -54,8 +18,6 @@ export default function SignUpForm() {
                                     type="email"
                                     name="email"
                                     placeholder="Enter your email address"
-                                    value={formData.email}
-                                    onChange={handleChange}
                                     required
                                 />
                             </div>
@@ -66,11 +28,9 @@ export default function SignUpForm() {
                                 <input 
                                     className="text-black peer block w-full rounded-md border border-gray-200"
                                     id="name"
-                                    type="text"
+                                    type="name"
                                     name="name"
-                                    placeholder="Enter your username"
-                                    value={formData.name}
-                                    onChange={handleChange}
+                                    placeholder="Enter your user name"
                                     required
                                 />
                             </div>
@@ -84,8 +44,6 @@ export default function SignUpForm() {
                                     type="password"
                                     name="password"
                                     placeholder="Enter your password"
-                                    value={formData.password}
-                                    onChange={handleChange}
                                     required
                                 />
                             </div>
