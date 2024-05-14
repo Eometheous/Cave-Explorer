6553c8681239b6f7964b755e9e9cea0ec8dfc292
Eometheous <jonathan.sthomas@icloud.com>
2024-05-13 23:28:55 -0700

    fixed formatting

--- dev/web_app/react/cave-explorer/app/pages/about/page.tsx
+++ dev/web_app/react/cave-explorer/app/pages/about/page.tsx
@@ -1,12 +1,13 @@
 export default function about() {
     return (
-    <main>
-        <h1>About Cave Explorer</h1>
-        <p>
-            Cave Explorer is a simple sliding puzzle game where the objective is to move a square piece from the starting slot to the end slot. The game consists of a square grid where the user interacts by clicking and dragging the white square to the top of the level.</p>
-        <p>
-            As the player progresses, the game will gradually introduce more obstacles, such as blocks blocking the user's path, making the puzzles increasingly challenging.
-        </p>
-      </main>
-    )
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>About Cave Explorer</h1>
+            <p>
+                Cave Explorer is a simple sliding puzzle game where the objective is to move a square piece from the starting slot to the end slot. The game consists of a square grid where the user interacts by clicking and dragging the white square to the top of the level.
+            </p>
+            <p>
+                As the player progresses, the game will gradually introduce more obstacles, such as blocks blocking the user's path, making the puzzles increasingly challenging.
+            </p>
+        </main>
+    );
 }
