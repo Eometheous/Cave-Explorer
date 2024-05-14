65af5e55f1812a7affe6c3074bae17c033764607
Eometheous <jonathan.sthomas@icloud.com>
2024-05-13 22:21:25 -0700

    updated itch build

--- dev/web_app/react/cave-explorer/app/pages/dashboard/page.tsx
+++ dev/web_app/react/cave-explorer/app/pages/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function game() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <iframe src="https://itch.io/embed-upload/10411748?color=333333" width="960" height="660"></iframe>
+            <iframe src="https://itch.io/embed-upload/10418467?color=333333" width="960" height="660"></iframe>
         </main>
     );
 }
